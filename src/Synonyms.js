0e973939ea9f8e3b79fd45f1d691bb86c504d6dc
RoCristal <mail@rominacristaldo.com>
2022-04-02 19:05:50 +0200

    Updated Synonym component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  //console.log(props.synonyms);
-  if (props.synonyms) {
-    return "Synonyms";
-  } else {
+  console.log(props.synonyms);
+  if (props.synonyms.lenght <= 1) {
     return null;
+  } else {
+    return (
+      <div>
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <span className="synonym" key={index}>
+              {" "}
+              {synonym}{" "}
+            </span>
+          );
+        })}
+      </div>
+    );
   }
 }
