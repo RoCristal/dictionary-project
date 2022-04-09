67072d1d799cfcbf0caae54cf5f3d0ad3519572d
RoCristal <mail@rominacristaldo.com>
2022-04-09 13:09:00 +0200

    Modified footer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          Welcome to my dictionary made in Java Script and React.
-        </header>
+        <div className="wrapper">
+          <header className="App-header">
+            Welcome to my dictionary made in Java Script and React.
+          </header>
 
-        <main className="App-main">
-          <Dictionary />
-        </main>
+          <main className="App-main">
+            <Dictionary />
+          </main>
+        </div>
         <footer className="App-footer">
           <small>
             Coded by Romina - Open source code -
